756c5ecd64dc5eb648a202fe47fd9fbc26e35ec6
Madjid <harunamadjid226@gmail.com>
2025-04-13 05:06:06 +0200

    Create and run the server

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -40,7 +40,11 @@ export default function Header() {
           <FaMoon />
         </Button>
         <Link to="/sign-in">
-          <Button className="bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 text-white hover:bg-gradient-to-br focus:ring-blue-300 dark:focus:ring-blue-800">
+          <Button
+            outline
+            gradientDuoTone="purpleToBlue"
+            //className="bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 text-white hover:bg-gradient-to-br focus:ring-blue-300 dark:focus:ring-blue-800 "
+          >
             Sign In
           </Button>
         </Link>
