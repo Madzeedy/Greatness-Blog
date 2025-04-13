ec736494298c0c4464cfe6f9bb452f4263677fc4
Madjid <harunamadjid226@gmail.com>
2025-04-13 04:44:55 +0200

    Create a header

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,62 @@
+import {
+  Button,
+  Navbar,
+  NavbarCollapse,
+  NavbarLink,
+  NavbarToggle,
+  TextInput,
+} from "flowbite-react";
+import { Link, useLocation } from "react-router-dom";
+import { AiOutlineSearch } from "react-icons/ai";
+import { FaMoon } from "react-icons/fa";
+
+export default function Header() {
+  const path = useLocation().pathname;
+
+  return (
+    <Navbar className="border-b-2 ">
+      <Link
+        to="/"
+        className="self-center whitespace-nowrap text-sm sm:text-xl dark:text-white font-semibold"
+      >
+        <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
+          GreatNess
+        </span>
+        Blogs
+      </Link>
+      <form>
+        <TextInput
+          type="text"
+          placeholder="Search..."
+          rightIcon={AiOutlineSearch}
+          className="hidden lg:inline"
+        />
+      </form>
+      <Button className="w-12 h-10 lg:hidden" color="gray" pill>
+        <AiOutlineSearch />
+      </Button>
+      <div className="flex gap-2 md:order-2">
+        <Button className="w-12 h-10 hidden sm:inline" color="gray" pill>
+          <FaMoon />
+        </Button>
+        <Link to="/sign-in">
+          <Button className="bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 text-white hover:bg-gradient-to-br focus:ring-blue-300 dark:focus:ring-blue-800">
+            Sign In
+          </Button>
+        </Link>
+        <NavbarToggle />
+      </div>
+      <NavbarCollapse>
+        <NavbarLink active={path === "/"} as={"div"}>
+          <Link to="/">Home</Link>
+        </NavbarLink>
+        <NavbarLink active={path === "/about"} as={"div"}>
+          <Link to="/about">About</Link>
+        </NavbarLink>
+        <NavbarLink active={path === "/projects"} as={"div"}>
+          <Link to="/projects">Projects</Link>
+        </NavbarLink>
+      </NavbarCollapse>
+    </Navbar>
+  );
+}
