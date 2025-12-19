85e541e98a98763c2fb83ff612ef9025d166b9de
Madjid <harunamadjid226@gmail.com>
2025-12-19 07:13:48 +0200

    Adding modifications to the footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,15 +3,22 @@ import {
   FooterLinkGroup,
   FooterLink,
   FooterTitle,
+  FooterDivider,
+  FooterCopyright,
+  FooterIcon,
 } from "flowbite-react";
 import { Link } from "react-router-dom";
+import { BsFacebook, BsInstagram, BsTwitter, BsGithub } from "react-icons/bs";
 
 export default function FooterComponent() {
   return (
-    <Footer container className="border border-t-8 border-teal-500 !bg-inherit">
-      <div className="">
-        <div className="w-full text-center  mb-4 ">
-          <div className="">
+    <Footer
+      container
+      className="border border-t-8 border-teal-500' !bg-inherit"
+    >
+      <div className="w-full max-w-7xl mx-auto">
+        <div className="grid w-full justify-between sm:flex md:grid-cols-1">
+          <div className="mt-5">
             <Link
               to="/"
               className="self-center whitespace-nowrap text-lg sm:text-xl dark:text-black font-semibold"
@@ -22,21 +29,46 @@ export default function FooterComponent() {
               Blog
             </Link>
           </div>
-          <div className="grid grid-cols-2 gap-3 sm: mt-4 sm:grid-cols-3sm:gap-6 ">
-            <FooterTitle
-              className="dark:text-black font-semibold"
-              title="About"
-            />
-            <FooterLinkGroup col>
-              <FooterLink
-                className="dark:text-black"
-                href="https://www.100jsprojects.com"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                100 JS Projects
-              </FooterLink>
-            </FooterLinkGroup>
+          <div className="grid grid-cols-2 gap-8 mt-4 sm:grid-cols-3 sm:gap-6">
+            <div>
+              <FooterTitle
+                className="dark:text-black font-semibold"
+                title="About"
+              />
+              <FooterLinkGroup col>
+                <FooterLink
+                  className="dark:text-black"
+                  href="https://www.100jsprojects.com"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  100 JS Projects
+                </FooterLink>
+
+                <FooterLink
+                  className="dark:text-black"
+                  href="https://www.100jsprojects.com"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  100 JS Projects
+                </FooterLink>
+              </FooterLinkGroup>
+            </div>
+          </div>
+        </div>
+        {/*<FooterDivider /> */}
+        <div className="w-full sm:flex sm:items-center sm:justify-between">
+          <FooterCopyright
+            href="#"
+            by="GreatNess Blog"
+            year={new Date().getFullYear()}
+          />
+          <div className="flex gap-6 sm:mt-0 mt-4 sm:justify-center">
+            <FooterIcon href="#" icon={BsFacebook} />
+            <FooterIcon href="#" icon={BsInstagram} />
+            <FooterIcon href="#" icon={BsTwitter} />
+            <FooterIcon href="#" icon={BsGithub} />
           </div>
         </div>
       </div>
